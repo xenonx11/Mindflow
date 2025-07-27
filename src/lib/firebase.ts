25f51d0540e3c80a5c2551fcef5644bc49b699a4
Nothing <code.x1mail@gmail.com>
2025-07-27 11:14:41 +0000

    Console Error
    FirebaseError: Firebase: Error (auth/popup-closed-by-user

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, connectAuthEmulator, browserPopupRedirectResolver } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -14,7 +14,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+const auth = getAuth(app, {
+    persistence: undefined,
+    popupRedirectResolver: browserPopupRedirectResolver,
+});
+
+auth.tenantId = '__';
+
 const db = getFirestore(app);
 
 export { app, auth, db };
